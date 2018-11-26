f9368fe9f7dbe46ff60da976c656662dfd554a36
yangsh_8671 <yangsh_8671@163.com>
2018-11-26 10:36:07 +0800

    阿萨德

---
+++ XM_01/src/main/java/com/demo/jedis/JsonUtils.java
@@ -0,0 +1,69 @@
+package com.demo.jedis;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class JsonUtils {
+
+	 // ����jackson����
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    /**
+     * ������ת����json�ַ�����
+     * <p>Title: pojoToJson</p>
+     * <p>Description: </p>
+     * @param data
+     * @return
+     */
+    public static String objectToJson(Object data) {
+    	try {
+			String string = MAPPER.writeValueAsString(data);
+			return string;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	return null;
+    }
+    
+    /**
+     * ��json�����ת��Ϊ����
+     * 
+     * @param jsonData json����
+     * @param clazz �����е�object����
+     * @return
+     */
+    public static <T> T jsonToPojo(String jsonData, Class<T> beanType) {
+        try {
+            T t = MAPPER.readValue(jsonData, beanType);
+            return t;
+        } catch (Exception e) {
+        	e.printStackTrace();
+        }
+        return null;
+    }
+    
+    /**
+     * ��json����ת����pojo����list
+     * <p>Title: jsonToList</p>
+     * <p>Description: </p>
+     * @param jsonData
+     * @param beanType
+     * @return
+     */
+    public static <T>List<T> jsonToList(String jsonData, Class<T> beanType) {
+    	JavaType javaType = MAPPER.getTypeFactory().constructParametricType(List.class, beanType);
+    	try {
+    		List<T> list = MAPPER.readValue(jsonData, javaType);
+    		return list;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	
+    	return null;
+    }
+}
