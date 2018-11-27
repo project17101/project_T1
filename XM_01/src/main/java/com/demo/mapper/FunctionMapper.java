8dc5c866f9141fb40d846660c54d0e1ee44f174a
yangsh_8671 <yangsh_8671@163.com>
2018-11-27 15:47:47 +0800

    1.0.1

---
+++ XM_01/src/main/java/com/demo/mapper/FunctionMapper.java
@@ -0,0 +1,10 @@
+package com.demo.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+public interface FunctionMapper {
+
+	public List<Map> findMenu(String role_id);
+	
+}
---
+++ XM_01/src/main/java/com/demo/service/interfaces/FunctionService.java
@@ -0,0 +1,10 @@
+package com.demo.service.interfaces;
+
+import java.util.List;
+import java.util.Map;
+
+public interface FunctionService {
+
+	public List<Map> findMenu(String role_id);
+	
+}
