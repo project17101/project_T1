fd354db1ae79869714415f862bdaf57f93c3fce1
yangsh_8671 <yangsh_8671@163.com>
2018-11-28 11:08:48 +0800

    11

---
+++ XM_01/src/main/java/com/demo/model/PageBean.java
@@ -0,0 +1,20 @@
+package com.demo.model;
+
+public class PageBean {
+	
+	  private Integer startrow;
+	  private Integer pagesize;
+	public Integer getStartrow() {
+		return startrow;
+	}
+	public void setStartrow(Integer startrow) {
+		this.startrow = startrow;
+	}
+	public Integer getPagesize() {
+		return pagesize;
+	}
+	public void setPagesize(Integer pagesize) {
+		this.pagesize = pagesize;
+	}
+
+}
